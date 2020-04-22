99bfe28b8d361c642e11743f9bd2abcf40d0d648
Miller <MMille014@cable.comcast.com>
2020-04-22 12:02:51 -0400

    Just starting

---
+++ src/app/satellite.ts
@@ -0,0 +1,16 @@
+export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+}
+
+function OrbitReport (name, orbitType, type, operational, launchDate) {
+    this.name: string = name; 
+    this.type: string = type; 
+    this.launchDate: string; 
+    this.orbitType: string; 
+    this.operational: boolean)
+}
+
