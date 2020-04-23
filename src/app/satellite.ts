bb54e0ea2152a76857726bb4776b46261a7ce6bd
Miller <MMille014@cable.comcast.com>
2020-04-23 12:25:23 -0400

    working on 8

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,15 +1,20 @@
 export class Satellite {
-    name: string;
-    orbitType: string;
-    type: string;
-    operational: boolean;
-    launchDate: string;
+   name: string;
+   orbitType: string;
+   type: string;
+   operational: boolean;
+   launchDate: string;
 
-constructor(name:string, type:string, launchDate:string, orbitType:string, operational:boolean) {
-    this.name = name; 
-    this.type = type;
-    this.launchDate = launchDate;
-    this.orbitType = orbitType;        
-    this.operational = operational;    
-}
+   constructor(name:string, type:string, launchDate:string, orbitType:string, operational:boolean) {
+      this.name = name; 
+      this.type = type;
+      this.launchDate = launchDate;
+      this.orbitType = orbitType;        
+      this.operational = operational;    
+    }
+
+    shouldShowWarning() {
+        return (this.type === "Space Debris");
+    }
+
 }
